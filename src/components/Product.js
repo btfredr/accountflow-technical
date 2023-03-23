ad5cc69c183ec2c4a8760eb9889f09d6676da7e4
Fredrik <fredrikf2000@gmail.com>
2023-03-23 10:45:30 +0100

    working on products and cart functions

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,7 +1,20 @@
 import React from "react";
 
-const Product = () => {
-  return <div>Product</div>;
+const Product = ({ product }) => {
+  return (
+    <div className="product">
+      <img src={product.images[0]} alt={product.description} />
+      <div className="product__content">
+        <h4>{product.title}</h4>
+        <p>{product.price}</p>
+        <div className="product__btnContainer">
+          <button>
+            <a href=""></a>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Product;
